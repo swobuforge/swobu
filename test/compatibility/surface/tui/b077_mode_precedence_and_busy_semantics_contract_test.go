23ba879e085fdbac0bf977b5a603aa2b85003877
Your Name <you@example.com>
2026-04-30 15:49:01 +0100

    Remove remaining first-run focus fragility in compatibility tests

--- test/compatibility/surface/tui/b077_mode_precedence_and_busy_semantics_contract_test.go
+++ test/compatibility/surface/tui/b077_mode_precedence_and_busy_semantics_contract_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestB077_ModePrecedence_EditModeConsumesTextInputBeforeNavTraversal(t *testing.T) {
 	journey := startJourney(t, 160, 50)
 	journey.WaitVisible("name")
-	journey.FocusRow("name")
+	journey.SendKey("down")
 	journey.ActivateFocusedRow()
 	journey.SendKey("_")
 	journey.WaitVisible("__")
